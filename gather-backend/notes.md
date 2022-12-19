9935c34e1a27087aa4aadccd358b6a21cb5d592a
jae <albertforus@gmail.com>
2022-12-19 15:57:02 -0500

    simple css

--- gather-backend/notes.md
+++ gather-backend/notes.md
@@ -1,2 +1,3 @@
 another working example: https://devdojo.com/bobbyiliev/quick-introduction-to-websockets-with-nodejs \
-publishing to public: https://youtu.be/RL_E56NPSN0
+publishing to public: https://youtu.be/RL_E56NPSN0 \
+styling: https://www.youtube.com/watch?v=nDkqSq2BXcc
--- gather-backend/server.js
+++ gather-backend/server.js
@@ -4,12 +4,8 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client){
-      if (client !== ws, client.readyState === WebSocket.OPEN) {
-        client.send(data,  { binary: isBinary });
-      }
-    })
+    [...wss.clients].filter(c => c !== ws)
+    .forEach(c => c.send(isBinary? data.toString(): data));
     console.log('received: %s', data);
+    })
   });
-
-});
