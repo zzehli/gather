9cadd74a29ae634fc16c21e729b5df3d3df3321d
jae <albertforus@gmail.com>
2022-12-20 23:26:29 -0500

    frontend login page and main page component setup

---
+++ gather-frontend/src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/Home";
+import "./style.scss";
+import Login from "./pages/Register";
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ gather-frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Sidebar
