ffdac7eecb28a4e5e02b6fa198e593c517fa60b5
jae <albertforus@gmail.com>
2023-02-04 23:36:52 -0500

    remove user name client local storage

--- gather-client/pages/index.js
+++ gather-client/pages/index.js
@@ -2,7 +2,7 @@ import styles from "../styles/Home.module.css"
 import { useSocket } from '../context/socketContext'
 import Messages from '../components/Messages';
 import Rooms from '../components/Rooms';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 export default function Home() {
     const { username, setUsername } = useSocket();
@@ -15,15 +15,7 @@ export default function Home() {
         }
 
         setUsername(value);
-
-        localStorage.setItem("username", value);
     }
-
-    useEffect(() => {
-        if (usernameRef) {
-            usernameRef.current.value = localStorage.getItem('username') || ""
-        }
-    }, [])
 
     return (<div>
         {!username && (
